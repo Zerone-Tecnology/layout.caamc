1774acec13561575cf8f5a990e0cf895cd96be96
Damir <damir.dmr88@gmial.com>
2025-04-08 22:53:22 +0500

    0804

--- js/common.js
+++ js/common.js
@@ -1,6 +1,34 @@
 $(function() {
 
-  // Custom JS
-  // document.body.hidden = true // test
+	if (ScrollTrigger.isTouch !== 1) {
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+	
+	ScrollSmoother.create({
+		wrapper: '.main',
+		content: '.container',
+		smooth: 1.5,
+		effects: true
+	});
+	
+	gsap.fromTo('.main-content', {opacity: 1},{
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.main-content',
+			start: 'center',
+			end: '100',
+			scrub: true
+		}
+	});
+
+	gsap.fromTo('.cover-block-wrap', {opacity: 1},{
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.cover-block-wrap',
+			start: 'center',
+			end: '100',
+			scrub: true
+		}
+	});
+}
 
 });
